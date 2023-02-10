55c95fb18f7c7d12a6ca482b07dea5f29c1a8245
KT <au637855@uni.au.dk>
2023-02-10 01:24:24 +0100

    started making the sign up screen

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -2,9 +2,9 @@ import type { Component } from 'solid-js';
 
 const Home: Component = () => {
     return <>
-        <div class="flex mt-20 items-center justify-center">
-            <div class="flex flex-col border p-4 rounded-lg shadow-lg w-64">
-                <h1 class="text-2xl">Login.</h1>
+        <div class="container">
+            <div class="login-container">
+                <h1 class="login-text">Login.</h1>
                     <input
                         id="email"
                         class="mt-2 border p-2 rounded-sm"
